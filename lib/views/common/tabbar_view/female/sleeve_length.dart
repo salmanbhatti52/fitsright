36ba4529d64f923999b34a7b9e067927929cefeb
Areeb Rajwana <56582057+areebrajwana@users.noreply.github.com>
2023-01-10 11:37:48 +0500

    update

---
+++ lib/views/common/tabbar_view/female/sleeve_length.dart
@@ -0,0 +1,61 @@
+import 'package:fits_right/utils/app_colors.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+import '../../dialouges/sleeve_length_dialog.dart';
+
+class SleeveLengthTabBarView extends StatefulWidget {
+  const SleeveLengthTabBarView({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  State<SleeveLengthTabBarView> createState() => _SleeveLengthTabBarViewState();
+}
+
+class _SleeveLengthTabBarViewState extends State<SleeveLengthTabBarView> {
+  @override
+  Widget build(BuildContext context) {
+    var size = MediaQuery.of(context).size;
+    return Stack(
+      children: [
+        Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text(
+                'Sleeve Length',
+                style: textStyle(FontWeight.w600, AppColors.commonBtnColor,
+                    size.height * 0.018),
+              ),
+            ),
+          ],
+        ),
+        Expanded(
+          child: SizedBox(
+            child: Image.asset('assets/pngs/sleevelength_female.png'),
+          ),
+        ),
+        Positioned(
+          right: 10,
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: GestureDetector(
+                onTap: () {
+                  showDialog(
+                    context: context,
+                    builder: (context) => const SleeveLengthDialog(),
+                  );
+                },
+                child: SvgPicture.asset('assets/svgs/informationicon.svg')),
+          ),
+        ),
+      ],
+    );
+  }
+
+  TextStyle textStyle(FontWeight weight, Color color, double size) {
+    return GoogleFonts.inter(fontWeight: weight, color: color, fontSize: size);
+  }
+}
